32e9f26d28d2b83212f7df6d7187b57fe1029f59
miguel sosa <miguel.sosa@agilethought.com>
2021-07-21 13:39:07 -0500

    Commit inicial

---
+++ poc-backbase/src/main/java/com/anzen/pockbackbase/PocBackbaseApplication.java
@@ -0,0 +1,13 @@
+package com.anzen.pockbackbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBackbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBackbaseApplication.class, args);
+	}
+
+}
---
+++ poc-backbase/src/test/java/com/anzen/pockbackbase/PocBackbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anzen.pockbackbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBackbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7e448eeeab0496aeaddfc2f4758daf4e9eec637d
ndeblas <noe.deblas@agilethought.com>
2021-07-29 15:39:13 -0500

    Restlet reached with camelContext tag.

--- poc-backbase/src/main/java/com/anzen/pockbackbase/PocBackbaseApplication.java
+++ poc-backbase/src/main/java/com/anzen/pockbackbase/PocBackbaseApplication.java
@@ -1,13 +1,46 @@
 package com.anzen.pockbackbase;
 
+import org.apache.camel.component.restlet.RestletComponent;
+import org.restlet.ext.spring.SpringServerServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+import org.restlet.Component;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@ImportResource("classpath:backbase-integration-service.xml")
 public class PocBackbaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocBackbaseApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean restletServletBean() {
+
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new SpringServerServlet(),
+				"/rs/*");
+		Map<String, String> initParams = new HashMap<>(1);
+		initParams.put(SpringServerServlet.Component_BEAN_PARAM_NAME, "restletComponent");
+		servletRegistrationBean.setInitParameters(initParams);
+		servletRegistrationBean.setName("RestletServlet");
+		servletRegistrationBean.setLoadOnStartup(1);
+		System.out.println("Este es el servlet---" + servletRegistrationBean.getServletName());
+		return servletRegistrationBean;
+	}
+
+	@Bean
+	public Component restletComponent() {
+		return new Component();
+	}
+
+	@Bean
+	public RestletComponent restletComponentService() {
+		return new RestletComponent(restletComponent());
+	}
 }
